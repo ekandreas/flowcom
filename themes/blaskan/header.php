47f73a6143b0f60683ebbc20a9805b292635a563
Andreas Ek <andreas@flowcom.se>
2013-05-30 11:31:25 +0200

    Spårningskod

--- themes/blaskan/header.php
+++ themes/blaskan/header.php
@@ -12,6 +12,19 @@
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 </head>
 <body <?php body_class(); ?>>
+<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-10747578-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+</script>
 <?php echo blaskan_top(); ?>
 <div id="site">
 	<div id="wrapper">
